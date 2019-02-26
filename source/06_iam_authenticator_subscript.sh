3e92e1b191d2f082c042ad907c6e8a898011440e
Filipsk <filip.skalka@integromat.com>
2019-02-26 13:12:08 +0100

    Bug repair

---
+++ source/06_iam_authenticator_subscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euox pipefail
+IFS=$'\n\t'
+
+apt-get -y install curl
+
+curl --location --silent --output /tmp/aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.11.5/2018-12-06/bin/linux/amd64/aws-iam-authenticator
+chmod +x /tmp/aws-iam-authenticator
+mv /tmp/aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
