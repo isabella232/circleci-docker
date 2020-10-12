48c47e8438e3eb5afd3acef68e15bdcc187bfc28
LukasRajter <39154625+LukasRajter@users.noreply.github.com>
2020-10-12 18:02:39 +0200

    Update 02_python_subscript.sh

--- source/02_python_subscript.sh
+++ source/02_python_subscript.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 apt-get -y install python python-pip
 pip install --upgrade pip setuptools
 
-apt-get install python3-pip
+apt-get -y install python3-pip
 pip3 install --upgrade pyinstaller
 
 rm -r /root/.cache
